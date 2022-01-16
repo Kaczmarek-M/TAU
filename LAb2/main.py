c6158bd650c99588c6ed86b36321790453bea83d
Kaczmarek-M <52320814+Kaczmarek-M@users.noreply.github.com>
2022-01-16 10:25:05 +0100

    Update main.py

--- LAb2/main.py
+++ LAb2/main.py
@@ -1,1 +1,17 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path='C:\chromedriver 97.0.4692.71\chromedriver.exe')
+
+driver.get('https://www.x-kom.pl')
+driver.maximize_window()
+
+check = driver.find_element(By.LINK_TEXT, "Pomoc i kontakt")
+check.click()
+
+check = driver.find_element(By.LINK_TEXT, 'Najczęściej zadawane')
+check.click()
+
+driver.quit()
+© 2022 GitHub, Inc.
