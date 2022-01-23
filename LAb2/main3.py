97e948671cea286830366cdcba3f378dbd6dc272
Kaczmarek-M <52320814+Kaczmarek-M@users.noreply.github.com>
2022-01-23 20:32:21 +0100

    Update main3.py

--- LAb2/main3.py
+++ LAb2/main3.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='C:\chromedriver 97.0.4692.71\chromedriver.exe')
-driver.get('https://www.olx.pl/')
+driver.get('https://www.ceneo.pl/')
 driver.maximize_window()
 check = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[2]/div[3]/div[3]/a')
 check.click()
